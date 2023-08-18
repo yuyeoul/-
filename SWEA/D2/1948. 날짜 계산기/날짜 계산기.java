433afea79a87a569deaf205610ce03a5cfbd0cd3
배유열 <93958447+yuyeoul@users.noreply.github.com>
2023-08-18 15:59:11 +0900

    [D2] Title: 날짜 계산기, Time: 134 ms, Memory: 20,476 KB -BaekjoonHub

--- SWEA/D2/1948. 날짜 계산기/날짜 계산기.java
+++ SWEA/D2/1948. 날짜 계산기/날짜 계산기.java
@@ -1,48 +1,42 @@
-
 import java.util.Scanner;
 
 public class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int num = sc.nextInt();
+		//월 저장 배열
 		int[] mon = new int[num * 2];
+		//일 저장 배열
 		int[] day = new int[num * 2];
+		//1~12월의 끝날 배열
+		int[] dayArr= {0,31,28,31,30,31,30,31,31,30,31,30,31};
+		//월 일 저장
 		for (int i = 0; i < num * 2; i++) {
 			mon[i] = sc.nextInt();
 			day[i] = sc.nextInt();
 		}
+		//출력번호 변수
 		int tc=0;
 		for (int i = 0; i < mon.length; i += 2) {
 			tc++;
+			//날짜 저장 변수
 			int ans = 0;
+			//첫 달과 마지막 달이 같으면 날짜 끼리 빼고 +1
 			if (mon[i] == mon[i + 1]) {
 				ans = day[i + 1] - day[i] + 1;
+			//같지 않다면
 			} else {
+				
 				for (int j = mon[i]; j <= mon[i + 1]; j++) {
+					//첫 번째 달이라면 그 달의 마지막 요일에서 입력된 요일 차이 ans에 더하기
 					if (j == mon[i]) {
-						if (j == 2) {
-							ans+=28-day[i];
-						} else if (j == 4 || j == 6 || j == 9 || j == 11) {
-							ans+=30-day[i];
-						} else {
-							ans+=31-day[i];
-						}
+						ans+=dayArr[j]-day[i]+1;
+					//마지막 달이라면 그 달에 같이 입력된 요일  더하기 
 					} else if (j == mon[i + 1]) {
-						if (j == 2) {
-							ans+=day[i+1]+1;
-						} else if (j == 4 || j == 6 || j == 9 || j == 11) {
-							ans+=day[i+1]+1;
-						} else {
-							ans+=day[i+1]+1;
-						}
+						ans+=day[i+1];
 					} else {
-						if (j == 2) {
-							ans+=28;
-						} else if (j == 4 || j == 6 || j == 9 || j == 11) {
-							ans+=30;
-						} else {
-							ans+=31;
-						}
+					//중간에 껴있는 달이라면 그 달의 전체 날짜 더하기
+						ans+=dayArr[j];
 					}
 				}
 
