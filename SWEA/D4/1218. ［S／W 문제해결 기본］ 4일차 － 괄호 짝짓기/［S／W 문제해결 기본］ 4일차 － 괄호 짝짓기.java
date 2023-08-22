e295752c90646170c890f37bfd63c1e02e29f9d3
배유열 <93958447+yuyeoul@users.noreply.github.com>
2023-08-22 13:44:35 +0900

    [D4] Title: [S/W 문제해결 기본] 4일차 - 괄호 짝짓기, Time: 153 ms, Memory: 20,892 KB -BaekjoonHub

--- SWEA/D4/1218. ［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기/［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기.java
+++ SWEA/D4/1218. ［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기/［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기.java
@@ -5,18 +5,24 @@ import java.util.Scanner;
 public class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		//continue 지점 설정
 		test: for (int tc = 1; tc <= 10; tc++) {
+			//문자열 길이
 			int len = sc.nextInt();
+			//문자열 배열에 담기
 			String str[] = sc.next().split("");
+			//왼쪽 괄호 담아줄 list
 			List<String> lList = new ArrayList<>();
+			//길이가 홀수면 애초에 불가능
 			if (len % 2 != 0) {
 				System.out.println("#" + tc + " 0");
 				continue;
 			}
-
+			//배열 돌면서 왼쪽 괄호면 리스트에 추가
 			for (int i = 0; i < len; i++) {
 				if (str[i].equals("(") || str[i].equals("{") || str[i].equals("[") || str[i].equals("<")) {
 					lList.add(str[i]);
+				//아니라면 비교 후 짝이 맞으면 리스트에서 짝 제거 or continue로 다음 문자열 넘어가기
 				} else {
 					if(str[i].equals(")")) {
 						if(lList.get(lList.size()-1).equals("(")){
