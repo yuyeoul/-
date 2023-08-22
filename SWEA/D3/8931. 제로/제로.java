ca207fa3a53c0a5ef77be5639f01c58f686200a9
배유열 <93958447+yuyeoul@users.noreply.github.com>
2023-08-22 10:14:16 +0900

    [D3] Title: 제로, Time: 486 ms, Memory: 99,112 KB -BaekjoonHub

--- SWEA/D3/8931. 제로/제로.java
+++ SWEA/D3/8931. 제로/제로.java
@@ -10,15 +10,19 @@ public class Solution {
 		for(int tc=1;tc<=num;tc++) {
 			//숫자 개수
 			int N=sc.nextInt();
+			//숫자를 담을 리스트 생성
 			List<Integer> list = new ArrayList<Integer>();
+			//숫자가 0이 아니면 리스트에 추가
 			for(int i=0;i<N;i++) {
 				int S=sc.nextInt();
 				if(S!=0) {
 					list.add(S);
+				//0이라면 리스트 맨뒤값 삭제
 				}else {
 					list.remove(list.size()-1);
 				}
 			}
+			//더해주기
 			int sum=0;
 			for(int i=0;i<list.size();i++) {
 				sum+=list.get(i);
