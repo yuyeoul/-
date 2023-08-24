fac1597b5f3b58ed87bf1dedcc7bedc6de0fabfb
배유열 <93958447+yuyeoul@users.noreply.github.com>
2023-08-24 10:51:53 +0900

    [Gold V] Title: AC, Time: 1276 ms, Memory: 197636 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.java
+++ 백준/Gold/5430. AC/AC.java
@@ -37,10 +37,10 @@ public class Main {
 				String str = bf.readLine();
 				// 실행함수에 D가 없으면 그대로 출력
 				if (dlist.size() == 0) {
-					bw.write(str+"\n");
+					bw.write(str + "\n");
 					// D가 있다면 error
 				} else {
-					bw.write("error"+"\n");
+					bw.write("error" + "\n");
 				}
 				// 한 개라면
 			} else if (len == 1) {
@@ -52,13 +52,13 @@ public class Main {
 				list.add(Integer.parseInt(str1));
 				// 실행 함수 D가 1개보다 많다면 error
 				if (dlist.size() > 1) {
-					bw.write("error"+"\n");
+					bw.write("error" + "\n");
 				} else {
 					// 아니라면 실행함수 실행
 					if (dlist.size() == 1) {
 						list.remove();
 					}
-					bw.write(list+"\n");
+					bw.write(list + "\n");
 				}
 				// 그 이상이라면
 			} else {
@@ -67,51 +67,55 @@ public class Main {
 				for (int i = 0; i < arr2.length; i++) {
 					list.add(Integer.parseInt(arr2[i]));
 				}
-				//R의 상태
-				//뒤에서 뺄지 앞에서 뺄지 정해줘야함
+				// R의 상태
+				// 뒤에서 뺄지 앞에서 뺄지 정해줘야함
 				int flag = 1;
 				boolean tf = true;
-				// 실행 함수 D의 개수가 숫자의 수보다 많다면 error
-				
-					for (int i = 0; i < arr.length; i++) {
-						//list안에 아무것도 없는데 D가 나온다면
-						if (list.size() == 0 && arr[i].equals("D")) {
-							tf = false;
-							break;
-						}
-						if (arr[i].equals("D")) {
-							if (flag == 1) {
-								list.remove();
-							} else {
-								list.removeLast();
-							}
-						} else {
-							flag *= -1;
-						}
+				for (int i = 0; i < arr.length; i++) {
+					// list안에 아무것도 없는데 D가 나온다면 tf=false->break
+					if (list.size() == 0 && arr[i].equals("D")) {
+						tf = false;
+						break;
 					}
-
-					if (tf == true) {
-						if (list.size() == 0) {
-							bw.write(list+"\n");
+					//실행함수 D이면 flag에 따라서 앞or뒤로 지워주기
+					if (arr[i].equals("D")) {
+						if (flag == 1) {
+							list.remove();
 						} else {
-							if (flag == 1) {
-								bw.write("[");
-								while(list.size()>1) {
-									bw.write(list.poll() + ",");
-								}
-								bw.write(list.peek() + "]\n");
-							} else {
-								bw.write("[");
-								while(list.size()>1) {
-									bw.write(list.pollLast() + ",");
-								}
-								bw.write(list.peekLast() + "]\n");
-							}
+							list.removeLast();
 						}
+					//R이라면 flag바꿔주기
 					} else {
-						bw.write("error"+"\n");
+						flag *= -1;
 					}
-				
+				}
+				//tf가 true라면
+				if (tf == true) {
+					//리스트가 비어있으면 []만 출력
+					if (list.size() == 0) {
+						bw.write(list + "\n");
+					//아니라면 flag에 따라서 앞에서부터 출력 or뒤에서부터 출력
+					} else {
+						if (flag == 1) {
+							bw.write("[");
+							while (list.size() > 1) {
+								bw.write(list.poll() + ",");
+							}
+							bw.write(list.peek() + "]\n");
+						} else {
+							bw.write("[");
+							while (list.size() > 1) {
+								bw.write(list.pollLast() + ",");
+							}
+							bw.write(list.peekLast() + "]\n");
+						}
+					}
+				//tf==false
+				//error
+				} else {
+					bw.write("error" + "\n");
+				}
+
 			}
 
 		}
