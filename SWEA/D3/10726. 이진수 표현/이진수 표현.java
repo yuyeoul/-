fbd803ae052363d7f5b72c84c5ba18deb89daaa4
배유열 <93958447+yuyeoul@users.noreply.github.com>
2023-08-29 11:54:16 +0900

    [D3] Title: 이진수 표현, Time: 339 ms, Memory: 51,600 KB -BaekjoonHub

--- SWEA/D3/10726. 이진수 표현/이진수 표현.java
+++ SWEA/D3/10726. 이진수 표현/이진수 표현.java
@@ -6,11 +6,15 @@ public class Solution {
 		//테케 개수
 		int T=sc.nextInt();
 		for(int tc=1;tc<=T;tc++) {
+			//연속되야하는 비트개수
 			int N=sc.nextInt();
+			//10진수
 			int M=sc.nextInt();
-			
+			//연속되는 비트개수 누적 변수
 			int cnt=0;
 			while(true) {
+				//십진수를 이진수로 만드는 방법->2로 나눠서 나머지를 기록한 다음에 역순으로 읽으면 된다
+				//따라서 1이 연속으로 N번 나오면 당첨
 				if(M%2==1) {
 					M=M/2;
 					cnt++;
