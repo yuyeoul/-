ca695d37c11cb4da05739955656265566e5fc865
배유열 <93958447+yuyeoul@users.noreply.github.com>
2023-09-08 23:28:42 +0900

    [D3] Title: 햄버거 다이어트, Time: 190 ms, Memory: 31,776 KB -BaekjoonHub

---
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+ 
+class Solution
+{
+    public static void main(String args[]) throws Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for(int i=1;i<=T;i++) {
+            int num=sc.nextInt();
+            int maxcal=sc.nextInt();
+            int[] sco = new int[num+1];
+            int[] cal = new int[num+1];
+            int[][] dp =new int [num+1][maxcal+1];
+            for(int j=1;j<=num;j++) {
+                sco[j]=sc.nextInt();
+                cal[j]=sc.nextInt();
+            }
+            for(int k=1;k<dp.length;k++) {
+                for(int j=0;j<dp[k].length;j++) {
+                    if(cal[k]>j) {
+                        dp[k][j]=dp[k-1][j];
+                    }else {
+                        dp[k][j]=Math.max(sco[k]+dp[k-1][j-cal[k]], dp[k-1][j]);
+                    }
+                }
+            }
+            System.out.println("#"+i+" "+dp[num][maxcal]);
+        }
+         
+         
+    }
+}
