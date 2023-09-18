009e16e91ceb6017211153d4c541f359b218f235
배유열 <93958447+yuyeoul@users.noreply.github.com>
2023-09-18 15:32:09 +0900

    [Silver I] Title: IOIOI, Time: 152 ms, Memory: 13420 KB -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -6,28 +6,27 @@ public class Main {
 		int N= sc.nextInt();
 		int len=sc.nextInt();
 		String str = sc.next();
-		String p="IOI";
-		for(int i=0;i<N-1;i++) {
-			p+="OI";
-		}
+		
 		int start=0;
-		int cnt=0;
+		int ans=0;
 		for(int i=start;i<len;i++) {
-			String ans="";
+			int cnt=0;
 			if(str.charAt(i)=='I') {
-				for(int j=i;j<=i+p.length()-1;j++) {
-					if(j>=len) {
+				for(int j=i+1;j<=i+N*2;j+=2) {
+					if(j+1<len && str.charAt(j)=='O'&&str.charAt(j+1)=='I') {
+						cnt++;
+					}else {
 						break;
 					}
-					ans+=str.charAt(j);
+					
 				}
 			}
-			if(ans.equals(p)) {
-				cnt++;
+			if(cnt==N) {
+				ans++;
 			}
 			start=i;
 		}
-		System.out.println(cnt);
+		System.out.println(ans);
 	}
 
 }
