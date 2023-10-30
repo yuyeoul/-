31e9769b6af164d2f0a17efe226dac7e25522a03
배유열 <93958447+yuyeoul@users.noreply.github.com>
2023-10-30 15:24:50 +0900

    [Silver III] Title: 주유소, Time: 876 ms, Memory: 160816 KB, Score: 58 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main{
+public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -19,27 +19,25 @@ public class Main{
 				mini=i;
 			}
 		}
-		city[N-1]=sc.nextInt();
-		int ans=0;
+		long ans=0;
 		for(int i=0;i<mini;) {
 			if(city[i]>city[i+1]) {
 				ans+=cost[i]*city[i];
 				i++;
 			}else {
 				int c=city[i];
-				int cnt=0;
+				long cnt=0;
 				while(true) {
 					cnt+=cost[i];
 					i++;
-					if(city[i]>city[i+1]) {
+					if(city[i]<=c) {
 						break;
 					}
 				}
 				ans+=c*cnt;
-				System.out.println(ans);
 			}
 		}
-		int cnt=0;
+		long cnt=0;
 		for(int i=mini;i<N-1;i++) {
 			cnt+=cost[i];
 		}
